91fb714d0ab88c9314f601fab3108263dcce664e
Shumon Huque <shuque@upenn.edu>
2012-06-10 10:33:01 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='check-aaaa',
+      version='0.1',
+      scripts=['check-aaaa'],
+      description='Check AAAA responses for set of DNS names',
+      author='Shumon Huque',
+      author_email='shuque@upenn.edu',
+      url='http://www.huque.com/software/check-aaaa/',
+
+      long_description = \
+      """checkinfo is a python program to check AAAA responses for a set of DNS names.""",
+      )
