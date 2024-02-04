083a3edc95bfb12f947c75b0f0188c2a5addb8b3
Joerg Kastning <jkastning@my-it-brain.de>
2024-02-04 21:48:55 +0100

    Ported to Python 3.x and customized it
    I forked the project and edited the script to make it run with
    Python 3.x. And I added some FQDNs to the list which I like to check
    periodically if they return an AAAA record or not.
    
    Signed-off-by: Joerg Kastning <jkastning@my-it-brain.de>

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='check-aaaa',
-      version='0.3',
+      version='0.4',
       scripts=['check-aaaa'],
       description='Check AAAA responses for set of DNS names',
-      author='Shumon Huque',
-      author_email='shuque@upenn.edu',
-      url='http://www.huque.com/software/check-aaaa/',
+      author='Joerg Kastning',
+      author_email='jkastning -@- my-it-brain.de',
+      url='https://github.com/Tronde/check-aaaa',
 
       long_description = \
       """checkinfo is a python program to check AAAA responses for a set of DNS names.""",
